923ff0305d205b32d8dd2418f697b14faa1be6a0
amclub2017 <peeb888@gmail.com>
2023-04-11 12:26:21 +0700

    commit

--- src/v1/auth/service.js
+++ src/v1/auth/service.js
@@ -56,8 +56,8 @@ const axios = require('axios');
 exports.loginGame = async (data) => {
     let jwtkey = 'eyJhbGciOiJIUzI1NiJ9.eyJJc3N1ZXIiOiJza19hZG1pbiIsIlVzZXJuYW1lIjoic29mdGtpbmdkb20ifQ.7uHUzTIGGhetySt1C6RHXd_bqZorOk1kw8CxfaluzjY'
     //verify the token
-    const token = req.headers.authorization.split(' ')[1];
-    // const  token = data.token;
+    // const token = req.headers.authorization.split(' ')[1];
+    const  token = data.token;
     const decoded = jwt.verify(token, jwtkey);
 
     const profile = await axios.get('https://member-api.angpaos.cloud/user/profile',{headers : {Authorization: `Bearer ${token}`}});
