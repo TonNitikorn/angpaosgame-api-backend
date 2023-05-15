72ca4b65f28586e5729fa6267a8ffc89292490c2
jbsoftkingdoms <b.jaturon@softkingdoms.com>
2023-05-15 15:31:30 +0700

    update admin

--- src/v1/admins/controller.js
+++ src/v1/admins/controller.js
@@ -13,3 +13,46 @@ exports.register = async (req, res, next) => {
         next(error);
     }
 }
+
+//get admin list
+exports.getAdminList = async (req, res, next) => {
+    try {
+
+        const result = await adminServices.getAdminList(req.body);
+        res.status(200).json(
+            result,
+        );
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+//edit admin by uuid
+exports.editAdmin = async (req, res, next) => {
+    try {
+
+        const result = await adminServices.editAdmin(req.body);
+        res.status(200).json(
+            result,
+        );
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+//change status admin by uuid
+exports.changeStatus = async (req, res, next) => {
+    try {
+
+        const result = await adminServices.changeStatus(req.body);
+        res.status(200).json(
+            result,
+        );
+
+    } catch (error) {
+        next(error);
+    }
+}
